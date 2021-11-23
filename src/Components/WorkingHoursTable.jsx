a5581d7170ccf74d2531abdc666347d64f00aa39
‪Tomer Schwartz‬‏ <65711940+kap70tomer@users.noreply.github.com>
2021-11-23 20:23:39 +0200

    Create WorkingHoursTable.jsx
    added table to haome page shows working days

---
+++ src/Components/WorkingHoursTable.jsx
@@ -0,0 +1,15 @@
+import '../Styles/hoursTable.css';
+
+export default function WorkingHoursTable() {
+    return (
+        <>
+            <table>
+                <tr><th>Sunday</th><td>9am - 6pm</td></tr>
+                <tr><th>Monday</th><td>9am - 6pm</td></tr>
+                <tr><th>Tuesday</th><td>9am - 6pm</td></tr>
+                <tr><th>Wednesday</th><td>9am - 6pm</td></tr>
+                <tr><th>Thursday</th><td>9am - 6pm</td></tr>
+            </table>
+        </>
+    )
+}
