e157dfd0606d4dc992c440c2b8dadc5932404a20
‪Tomer Schwartz‬‏ <65711940+kap70tomer@users.noreply.github.com>
2021-11-23 22:11:33 +0200

    Update appRouter.jsx

--- src/Components/appRouter.jsx
+++ src/Components/appRouter.jsx
@@ -1,9 +1,9 @@
 
 import { Route, Routes } from 'react-router-dom';
 import AboutPage from '../Pages/AboutPage';
-import ContactPage from '../Pages/ContactPage';
+// import ContactPage from '../Pages/ContactPage';
 import HomePage from '../Pages/HomePage';
-import BookingPage from '../Pages/BookingPage';
+// import BookingPage from '../Pages/BookingPage';
 
 const AppRouter = ()=> {
     return(
@@ -14,9 +14,9 @@ const AppRouter = ()=> {
             {/* About me & App 'help' page - guide and info */}
             <Route path ="/about" element = {<AboutPage/>} />
             {/* Booking Page - form for sending booking tattoos requests from clients. */}
-            <Route path ="/booking" element= {<BookingPage/>}/>
+            {/* <Route path ="/booking" element= {<BookingPage/>}/> */}
             {/* Contact page - facebook ,git , whatsapp ,phone ... */}
-            <Route path ="/contact" element= {<ContactPage/>}/>
+            {/* <Route path ="/contact" element= {<ContactPage/>}/> */}
             {/* deafult path home - redirect all urls with no match */}
             <Route path ='*' element ={<HomePage/>} />
         </Routes>
