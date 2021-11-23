d85ba4be7cdd3738c21a09374aad2822cf19ae24
‪Tomer Schwartz‬‏ <65711940+kap70tomer@users.noreply.github.com>
2021-11-23 19:25:28 +0200

    Update LocationDisplay.jsx
    smaller display size

--- src/Components/LocationDisplay.jsx
+++ src/Components/LocationDisplay.jsx
@@ -4,7 +4,7 @@ export default function LocationDisplay(){
             <iframe 
                 title="buisness location" 
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3383.455701507547!2d34.93813331554752!3d32.002767530364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151d34983d260799%3A0xbe49d85e58b53760!2sLakhish%20St%2087%2C%20Shoham!5e0!3m2!1sen!2sil!4v1627402027092!5m2!1sen!2sil" 
-                width="720" height="500" loading="lazy"
+                width="420" height="420" loading="lazy"
             />
             <h2>
                 .שוהם, לכיש 87. מוזמנים להגיע בתאום מראש
