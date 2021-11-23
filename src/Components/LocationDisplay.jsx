834df02f48723c6dccbd3008975f44acaa674794
‪Tomer Schwartz‬‏ <65711940+kap70tomer@users.noreply.github.com>
2021-11-23 20:23:18 +0200

    Update LocationDisplay.jsx
    removes title

--- src/Components/LocationDisplay.jsx
+++ src/Components/LocationDisplay.jsx
@@ -6,9 +6,6 @@ export default function LocationDisplay(){
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3383.455701507547!2d34.93813331554752!3d32.002767530364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151d34983d260799%3A0xbe49d85e58b53760!2sLakhish%20St%2087%2C%20Shoham!5e0!3m2!1sen!2sil!4v1627402027092!5m2!1sen!2sil" 
                 width="420" height="420" loading="lazy"
             />
-            <h2>
-                .שוהם, לכיש 87. מוזמנים להגיע בתאום מראש
-            </h2>
         </div>
     )
 }
